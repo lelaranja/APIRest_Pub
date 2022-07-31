2aef4ded2cd21d52206d07f56a163624e5e4436e
lelaranja <leticiajlaranja@gmail.com>
2022-07-31 14:57:10 -0300

    add: criação de bd, banco de dados e rotas

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 import express from "express";
 import MenuController from "./controller/cardapio-controller.js";
 import TransactionsController from "./controller/transacao-controller.js";
+import SuppliersController from "./controller/fornecedor-controller.js";
 import cors from "cors";
 const port = 3000;
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.listen(port, () =>{
-    console.log(`Server online, address: http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server online, address: http://localhost:${port}`);
+});
 
 TransactionsController.routes(app);
-MenuController.routes(app)
+MenuController.routes(app);
+SuppliersController.routes(app);
