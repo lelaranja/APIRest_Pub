0c513d754ec2af0c1b08f57e45e29d520d10b496
Digoassun <digoassun@gmail.com>
2022-07-31 15:20:10 -0300

    Controller, DAO e Model atualizados com alterações de validacao

--- src/DAO/cardapio-dao.js
+++ src/DAO/cardapio-dao.js
@@ -6,6 +6,8 @@ class MenuDAO extends DAO{
     static queryInsert = 'INSERT INTO MENU (produto,valor) VALUES (?,?)'
     static queryUpdate = 'UPDATE MENU SET produto = ?, valor= ? WHERE produto = ?'
     static queryDelete = 'DELETE FROM MENU WHERE produto= ?'
+    static paramDel = 'produto'
+
 }
 
 export default MenuDAO
--- src/model/cardapio-model.js
+++ src/model/cardapio-model.js
@@ -1,7 +1,7 @@
 class CardapioModel {
-    constructor(produto, valor) {
-        this.produto = produto;
-        this.valor = valor;
+    constructor(requisicao) {
+        this.produto = requisicao.produto;
+        this.valor = requisicao.valor;
     }
 }
 
