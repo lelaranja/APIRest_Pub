e12fb4560e7feb8c2ef2f29c70205689245fc98b
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2022-08-01 09:32:42 -0300

    feat: crieias rotas de estoque com os verbos post e get

---
+++ src/controller/estoque-controller.js
@@ -0,0 +1,36 @@
+import { Database } from "../DAO/estoque-dao.js";
+
+
+
+class Estoque {
+    static estoque(app){
+    app.get('/estoque',(req,res)=>{
+    res.send();
+})
+    app.get('/estoque/:id',(req,res)=>{
+        
+    })
+    app.get('/estoque/:fabricante',(req,res)=>{
+
+    })
+    app.get('/estoque/:qtdProdutos',(req,res)=>{
+
+    });
+    app.get('/estoque/:nomeProduto',(req,res)=>{
+
+    })
+    app.post('/estoque/:id',(req,res)=>{
+
+    })
+    app.post('/estoque/:fabricante',(req,res)=>{
+
+    });
+    app.post('/estoque/:qtdProdutos',(req,res)=>{
+
+    })
+    app.post('/estoque/:nomeProduto',(req,res)=>{
+
+})
+}
+}
+export default Estoque;
