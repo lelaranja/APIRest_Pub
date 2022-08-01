bb5f4be3435a865abf0365cb6f76d86c146a4dd2
lelaranja <leticiajlaranja@gmail.com>
2022-08-01 13:40:00 -0300

    add: import da entidade de estoque

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 import express from "express";
-<<<<<<< HEAD
-import estoqueController from './controller/estoque-controller.js';
-
-=======
 import MenuController from "./controller/cardapio-controller.js";
 import TransactionsController from "./controller/transacao-controller.js";
 import SuppliersController from "./controller/fornecedor-controller.js";
+import estoqueController from "./controller/estoque-controller.js";
+
 import cors from "cors";
->>>>>>> master
 const port = 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-<<<<<<< HEAD
-estoqueController.routes(app)
-app.listen(port, () =>{
-    console.log(`Server online, address: http://localhost:${port}`)
-})
-=======
 app.listen(port, () => {
   console.log(`Server online, address: http://localhost:${port}`);
 });
@@ -27,4 +18,4 @@ app.listen(port, () => {
 TransactionsController.routes(app);
 MenuController.routes(app);
 SuppliersController.routes(app);
->>>>>>> master
+estoqueController.routes(app);
