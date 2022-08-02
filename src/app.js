a73ba2961d433dff5f3b34ae282ef8d6287c21b4
SGErik <erik.souzaxbr@gmail.com>
2022-08-02 00:03:59 -0300

    Refatoração da rota de funcionários

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import MenuController from "./controller/cardapio-controller.js";
 import TransactionsController from "./controller/transacao-controller.js";
 import SuppliersController from "./controller/fornecedor-controller.js";
 import estoqueController from "./controller/estoque-controller.js";
+import StaffControllers from "./controller/funcionario-controller.js";
 
 import cors from "cors";
 import autorizacao from "./middleware/valida-header.js"
@@ -18,6 +19,7 @@ app.listen(port, () => {
   console.log(`Server online, address: http://localhost:${port}`);
 });
 
+StaffControllers.routes(app);
 TransactionsController.routes(app);
 MenuController.routes(app);
 SuppliersController.routes(app);
