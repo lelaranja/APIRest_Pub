168e54cff85016eb80549004f3e1e8e08576f35d
Willian Carvalho da Silva <wcarvalho91610@gmail.com>
2022-08-02 17:18:06 -0300

    cirei a rota delete e o banco de dados

--- src/controller/estoque-controller.js
+++ src/controller/estoque-controller.js
@@ -1,88 +1,56 @@
-import EstoqueDao from "../DAO/estoque-dao.js";
-import estoqueDAO from "../DAO/estoque-dao.js";
+import EstoqueDAO from "../DAO/estoque-dao.js";
 import EstoqueModel from "../Model/estoque-model.js";
 import Validacoes from "../services/validacoes.js";
 
-
 class EstoqueController {
     static routes(app){
-    app.get('/estoque', async (req,res)=>{
+    app.get('/storage', async (req,res)=>{
         try{
-            const dados = await estoqueDAO.pegaTodosDados(req.params.estoque)
+            console.log("to aqui");
+            const dados = await EstoqueDAO.pegaTodosDados()
             res.status(dados.status).json(dados.resultado.msg)
         }catch(error){
             res.status(error.status).json(error.msg)
         }
 })
-    
-    app.get('/estoque/:fabricante',(req,res)=>{
+    app.get('/storage/nomeProduto/:nomeProduto',async (req,res)=>{
         try{
-            const response = estoqueDAO.pegaUmDado(req.params.fabricante);
-            res.status(response.status).json(response.msg)
-        }catch(error){
-            res.status(error.status).json(error.msg)
-        }    
-    })
-    app.get('/estoque/:qtdProdutos',async (req,res)=>{
-        try{
-            const response = await estoqueDAO.pegaUmDado(req.params.qtdProdutos);
-            res.status(response.status).json(response.msg)
-        }catch(error){
-            res.status(error.status).json(error.msg)
-        }
-    });
-    app.get('/estoque/:nomeProduto',async (req,res)=>{
-        try{
-            const response = await estoqueDAO.pegaUmDado(req.params.nomeProduto);
-            res.status(response.status).json(response.msg)
+            const response = await EstoqueDAO.pegaUmDado(req.params.nomeProduto);
+            res.status(response.status).json(response.resultado.msg)
         }catch(error){
             res.status(error.status).json(error.msg)
         }
     })
-    app.post('/estoque/',async (req,res)=>{
+    app.post('/storage',async (req,res)=>{
         try{
             const dados = new EstoqueModel(req.body)
             await Validacoes.reqIsEmpty(dados)
-            const response = await EstoqueDao.inserirDados(dados)
+            const response = await EstoqueDAO.inserirDados(dados)
             res.status(response.status).json(response.resultado.msg)
         }catch(error){
             res.status(error.status).json(error.msg)
         }
     })
-    app.put('/estoque/:fabricante',async(req,res)=>{
+    app.put('/storage/nomeProduto/:nomeProduto',async(req,res)=>{
         try{
             const dados = new EstoqueModel(req.body);
-            await Validacoes.notInBank(await EstoqueDao.pegaUmDado(req.params.fabricante))
+            await Validacoes.notInBank(await EstoqueDAO.pegaUmDado(req.params.nomeProduto))
             await Validacoes.reqIsEmpty(dados)
-            const response = await EstoqueDao.atualizarDado(dados, req.params.fabricante)
+            const response = await EstoqueDAO.atualizarDado(dados, req.params.nomeProduto)
             res.status(response.status).json(response.resultado.msg)
         }catch(error){
             res.status(error.status).json(error.msg)
         }
     })
-    app.put('/estoque/:qtdProdutos',async(req,res)=>{
-        try{
-            const dados = new EstoqueModel(req.body);
-            await Validacoes.notInBank(await EstoqueDao.pegaUmDado(req.params.qtdProdutos))
-            await Validacoes.reqIsEmpty(dados)
-            const response = await EstoqueDao.atualizarDado(dados, req.params.qtdProdutos)
-            res.status(response.status).json(response.resultado.msg)
-        }catch(error){
-            res.status(error.status).json(error.msg)
+    app.delete("/storage/nomeProduto/:nomeProduto", async (req, res) => {
+        try {
+            await Validacoes.notInBank(await EstoqueDAO.pegaUmDado(req.params.nomeProduto))
+            const resposta = await EstoqueDAO.deletaDado(req.params.nomeProduto)
+            res.status(resposta.status).json(resposta.resultado.msg);
+        } catch (error) {
+            res.status(error.status).json(error.msg);
         }
-    })
-    app.put('/estoque/:nomeProduto',async(req,res)=>{
-        try{
-            const dados = new EstoqueModel(req.body);
-            await Validacoes.notInBank(await EstoqueDao.pegaUmDado(req.params.nomeProduto))
-            await Validacoes.reqIsEmpty(dados)
-            const response = await EstoqueDao.atualizarDado(dados, req.params.nomeProduto)
-            res.status(response.status).json(response.resultado.msg)
-        }catch(error){
-            res.status(error.status).json(error.msg)
-        }
-    })
-    
- }
+    });
+}
 }
 export default EstoqueController;
