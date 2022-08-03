aedab3bae13521e97bc6db48cdef2955e4e7f840
igorsans <igor.santos0@hotmail.com>
2022-08-03 15:58:07 -0300

    Testes unitarios
    estruturado testes unitarios

--- test/unitario/scenarios.js
+++ test/unitario/scenarios.js
@@ -2,57 +2,60 @@ class Scenarios {
   static scenario = [
     //scenario 1
     {
-      msg: [
-        "dados vazios",
-        "dados invalidos com string vazia",
-        "dados invalidos com undefined",
-        "dados validos",
-      ],
+      msg: ["dados vazios", "dados validos"],
       dados: [
         [],
         [
           {
-            nome: "",
+            nome: "Estou preenchido",
             estado: "Rio de Janeiro",
             cidade: "Rio de Janeiro",
           },
         ],
-        [
-          {
-            nome: "Zaniel",
-            estado: undefined,
-            cidade: "Rio de Janeiro",
-          },
-        ],
-        [
-          {
-            nome: "Xizoma",
-            estado: "Xangai",
-            cidade: "Japao",
-          },
-        ],
       ],
+      toBe: [true, false],
     },
     //scenario 2
     {
-      msg: "teste com dados vazios",
+      msg: [
+        "Teste com dados vazios",
+        "Teste com dados vazios",
+        "Teste com dados vazios",
+        "Teste com dados completos",
+      ],
       dados: [
         {
           funcionario: "Rodolpho",
           produtos: "",
-          valorCompra: 1400
+          valorCompra: 1400,
         },
         {
           funcionario: "",
           produtos: "",
-          valorCompra: ""
+          valorCompra: "",
         },
         {
           funcionario: "Teste",
           produtos: "Testando",
-          valorCompra: ""
-        }
+          valorCompra: "",
+        },
+        {
+          funcionario: "Teste",
+          produtos: "Testando",
+          valorCompra: 400,
+        },
       ],
+      toBe: [true, true, true, false],
+    },
+    //scenario 3
+    {
+      msg: [
+        "Teste de string com string",
+        "Teste de string com string",
+        "Teste de string com Number",
+      ],
+      dados: ["Sou uma string", "Tambem sou", 400],
+      toBe: [true, true, false],
     },
   ];
 }
