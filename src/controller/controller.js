79bcda95e0f9e4e2a2fe097972345d182b6e083b
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-08-02 22:50:38 -0300

    refatoração de código para padronização da API

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,5 +1,6 @@
 import TransactionsController from "./transacao-controller.js";
 import MenuController from "./cardapio-controller.js";
 import SuppliersController from "./fornecedor-controller.js";
+import ClientController from "./cliente-controller.js";
 
-export { TransactionsController, MenuController, SuppliersController };
+export { TransactionsController, MenuController, SuppliersController, ClientController };
