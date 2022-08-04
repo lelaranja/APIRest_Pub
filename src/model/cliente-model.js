eafe4aa01cbbf3305c87b93f81c9cc7fdf6e0651
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-08-03 23:33:54 -0300

    rotas em funcionamento + validações feitas na model

--- src/model/cliente-model.js
+++ src/model/cliente-model.js
@@ -1,9 +1,33 @@
+import Validacoes from "../services/validacoes.js";
+import ErrosModel from "../model/errors-model.js";
+
 class ClienteModel {
-    constructor(requisicao) {
-        this.nome = requisicao.nome;
-        this.telefone = requisicao.telefone;
-        this.email = requisicao.email;
+    constructor(dados) {
+        this.nome = dados.nome;
+        this.telefone = dados.telefone;
+        this.email = dados.email;
     }
+
+    static validateModel(dados) {
+        return new Promise(async(resolve, reject) => {
+          try {
+            if (
+              Validacoes.isString(dados.nome) &&
+              Validacoes.isString(dados.email)
+            ) {
+              const modelado = new ClienteModel(dados);
+              await Validacoes.reqIsEmpty(modelado);
+              resolve(modelado);
+            } else
+              reject(
+                new ErrosModel("Alguns dados estão preenchidos incorretamente", 400)
+              );
+          } catch (error) {
+            reject(error)
+          }
+        });
+      }
 }
 
 export default ClienteModel;
+
